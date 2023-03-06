de5e85caf8add2877b2756b4d5da1720bf227c52
Yb-M <72449650@certus.edu.pe>
2023-03-05 20:28:52 -0500

    Se Agrego la Validación de Contraseñas y Correo

--- script.js
+++ script.js
@@ -20,3 +20,41 @@ btnPopup.addEventListener('click', () => {
 iconClose.addEventListener('click', () => {
     evolutara.classList.remove('active-popup');
 });
+
+// * Validar User y Contraseña
+// function validate() {
+//     var correo = document.getElementById("input-correo").value;
+//     var contra = document.getElementById("input-pass").value;
+//     if (correo == "ybermarlon@gmail.com" && contra == "62756606") {
+//         alert("Contraseña Validada Corréctamente. Se ingresó como ADMINISTRADOR");
+//         console.log(correo + " " + contra);
+//         window.location("./admin/index.html");
+//         return false;
+//     } else if (correo == "cliente@gmail.com" && contra == "cliente") {
+//         alert("Contraseña Validada Corréctamente. Se ingresó como CLIENTE");
+//         window.location("./catalogo.html");
+//         console.log(correo + " " + contra);
+//         return false;
+//     } else {
+//         alert("Error de Credenciales. Intente Nuevamente")
+//     }
+
+// }
+
+function validate() {
+    let user = document.getElementById('input-correo').value;
+    let clave = document.getElementById('input-pass').value;
+
+    if (user == "ybermarlon@gmail.com" && clave == "123") {
+        alert("Se Ingresó Como Administrador");
+        window.location = "./admin/index.html";
+
+        return false;
+    }
+    if (user == "user@gmail.com" && clave == "12") {
+        window.location = "./cliente/catalogo.html";
+        return false;
+    } else {
+        alert("Error de credenciales, inténtelo nuevamente.")
+    }
+}
